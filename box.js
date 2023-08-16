24a9fb937588a72979aed4d0f32d7ddb8f3146c6
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-08-15 19:29:44 -0500

    Delete box.js

--- box.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-            document.getElementById("image").style.backgroundImage = "url('" + localStorage.getItem('image') + "')";
-            document.getElementById("staturl").href = localStorage.getItem('staturl');
-            document.getElementById("appname").innerHTML = localStorage.getItem('appname');
-        });
